205ca484016a5ba6dd40275caa1abdf05391ab61
svetlana-urasinova <urasinova.work@gmail.com>
2022-01-27 19:00:28 +0100

    Update theme switcher: now saves state between sessions

--- js/classes/ThemeSwitcher.js
+++ js/classes/ThemeSwitcher.js
@@ -8,14 +8,38 @@ export class ThemeSwitcher {
 
     constructor() {
         this.#rootElem = document.querySelector('.app-root');
+        
+        // loads theme from localStorage if exists
+        this.updateTheme();
+
+        // add theme toggle handler to theme switch button
         this.#buttonElem = document.querySelector('.theme-switcher__btn');
         const clickHandler = () => this.toggleTheme();
         this.#buttonElem.addEventListener('click', clickHandler);
     }
 
-    toggleTheme() {
-        this.#themes.forEach(theme => {
-            this.#rootElem.classList.toggle(theme);
+    getCurrentTheme() {
+        return Number(localStorage.getItem('currentTheme')) ?? 0;
+    }
+
+    setCurrentTheme (value) {
+        localStorage.setItem('currentTheme', value);
+    }
+
+    updateTheme() {
+        const currentTheme = this.getCurrentTheme();
+        this.#themes.forEach((theme, i) => {
+            if (i === currentTheme) {
+                this.#rootElem.classList.add(theme);               
+            } else {
+                this.#rootElem.classList.remove(theme);
+            }
         });
+    }
+
+    toggleTheme() {
+        const newCurrentTheme = (this.getCurrentTheme() + 1) % this.#themes.length;   
+        this.setCurrentTheme(newCurrentTheme);
+        this.updateTheme();
     }
 }
